33d5966b3e651563a39617590eb54888f247a69c
NurzhanAuelbek <82052572+NurzhanAuelbek@users.noreply.github.com>
2023-07-11 02:16:09 +0600

    Добавьте файлы проекта.

---
+++ monte carlo/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace monte_carlo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(633, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PI";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(633, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Бастау";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(597, 153);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(191, 168);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label1.Location = new System.Drawing.Point(597, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "P(A|C): Ықтималдылық";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.label2.Location = new System.Drawing.Point(21, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(508, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "C = {Orange, Red, Green, Black, Yellow, Blue, Pink, Purple, Brown}";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(597, 328);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ monte carlo/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace monte_carlo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics g2 = CreateGraphics();
+            Random r = new Random();
+            Pen p1;
+
+
+            int krug = 0;
+            int tort = 0;
+
+
+            for (int i = 0; i < 10000; i++)
+            {
+                int rx = r.Next(50, 351);
+                int ry = r.Next(50, 351);
+
+                int radius = (int)Math.Sqrt(Math.Pow(rx - 200, 2) + Math.Pow(ry - 200, 2));
+                if (radius <= 150)
+                {
+                    p1 = new Pen(Color.Orange);
+                    krug++;
+                }
+                else
+                {
+                    p1 = new Pen(Color.Red);
+                    tort++;
+                }
+                g2.DrawRectangle(p1, rx, ry, 1, 1);
+            }
+            textBox1.Text = "PI = " + (double)(4 * krug) / 10000;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            int[,] squares = new int[3, 3];
+            Graphics g2 = CreateGraphics();
+            Random r = new Random();
+            Pen p1;
+
+            for(int i = 0; i < 10000; i++)
+            {
+                int rx = r.Next(50, 351);
+                int ry = r.Next(50, 351);
+
+                if (rx <= 150 && ry <= 150)
+                {
+                    p1 = new Pen(Color.Orange);
+                    squares[0, 0]++;
+                }
+                else if (rx > 150 && ry <= 150 && rx <= 250)
+                {
+                    p1 = new Pen(Color.Red);
+                    squares[0, 1]++; ;
+                }
+                else if (rx > 250 && ry <= 150 && rx <= 350)
+                {
+                    p1 = new Pen(Color.Green);
+                    squares[0, 2]++; ;
+                }
+                else if (rx <= 150 && ry <= 250 && ry > 150)
+                {
+                    p1 = new Pen(Color.Black);
+                    squares[1, 0]++; ;
+                }
+                else if (rx > 150 && ry <= 250 && rx <= 250)
+                {
+                    p1 = new Pen(Color.Yellow);
+                    squares[1, 1]++; ;
+                }
+                else if (rx > 250 && ry <= 250 && rx <= 350)
+                {
+                    p1 = new Pen(Color.Blue);
+                    squares[1, 2]++; ;
+                }
+                else if (rx <= 150 && ry <= 350 && ry > 250)
+                {
+                    p1 = new Pen(Color.Pink);
+                    squares[2, 0]++; ;
+                }
+                else if (rx <= 250 && rx > 150 || ry <= 250 && ry > 150)
+                {
+                    p1 = new Pen(Color.Purple);
+                    squares[2, 1]++; ;
+                }
+                else
+                {
+                    p1 = new Pen(Color.Brown);
+                    squares[2, 2]++;
+                }
+                g2.DrawRectangle(p1, rx, ry, 1, 1);
+            }
+
+            string res = "";
+            for (int i = 0; i < 3; i++)
+            {
+                res += "| ";
+                for (int j = 0; j < 3; j++)
+                {
+                    listView1.Items.Add(Math.Round((squares[i, j]) / 10000.0, 3).ToString());
+                }
+                res += "\n";
+            }
+
+        }
+        }
+}
